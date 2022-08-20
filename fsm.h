b08dd73d8edc90eb0ad0e9e830cf61623d9bc3cb
Mattia Cabrini <dev@mattiacabrini.com>
2022-08-20 12:07:01 +0200

    Update Copyright Notices

--- fsm.h
+++ fsm.h
@@ -1,3 +1,21 @@
+/*
+ *     FSM - Finite State Machine
+ *     Copyright (C) 2022  Mattia Cabrini <dev@mattiacabrini.com>
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 3.0 of the License, or (at your option) any later version.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this library.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef FSM_H_INCLUDED
 #define FSM_H_INCLUDED
 
